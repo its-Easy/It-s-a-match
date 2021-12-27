3d197251e41325d6053c93984550b07595f6f26b
Saral Srivastava <saralsrivastava25@gmail.com>
2021-12-27 22:56:45 +0530

    added timestamp

--- backend/models/user.posts.model.js
+++ backend/models/user.posts.model.js
@@ -3,10 +3,10 @@ const Schema = mongoose.Schema
 
 const postSchema = new Schema({
     email: String,
-    post_date: Array,
-    post_url: Array
-})
+    post_url: String]
+}, {timestamps: true})
 
+postSchema.index({createdAt:1}, {expireAfterSeconds: 86400})
 const post = mongoose.model('posts', postSchema)
 
 module.exports = post
