806d8d56e187bfd1cd02c018a3d55c338df34f8b
Saral Srivastava <saralsrivastava25@gmail.com>
2021-12-27 23:01:05 +0530

    created database for new user

--- backend/routers/user.signUp.route.js
+++ backend/routers/user.signUp.route.js
@@ -6,16 +6,35 @@ const crypto = require('crypto')
 let user = require('../models/user.auth.model')
 
 const twilioClient = require("twilio")(
-    process.env.TWILIO_ACCOUNT_SID,
-    process.env.TWILIO_AUTH_TOKEN
-);
-const verificationSID = process.env.TWILIO_VERIFY_SID;
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+  );
+  const verificationSID = process.env.TWILIO_VERIFY_SID;
   
-var newUser = {
-  email: "",
-  password: ""
-}
+  var newUser = {
+    email: "",
+    password: ""
+  }
+  
+  function createdb(email){
+    var key;
+    for(var i=0;i<email.length;i++) {
+      if(email.charAt(i)=='@'||email.charAt(i)=='.') {key=i;break;}
+    }
+    var name=email.slice(0, key)
 
+    var url = `mongodb+srv://saral:saral@cluster0.1eecc.mongodb.net/${name}`;
+    var MongoClient = require('mongodb').MongoClient;
+    MongoClient.connect(url, function(err, client) {
+      var db=client.db(`${name}`)
+      if (err) throw err;
+      db.createCollection("profileDetails", function(err, result) {
+        if(err) throw err;
+        console.log(`Database created for ${name}`)
+        client.close()
+      })
+    });
+}
 router.route('/').post((req, res) => {
   const email=req.body.email
   const md5sum = crypto.createHash('md5')
@@ -23,13 +42,12 @@ router.route('/').post((req, res) => {
   
   newUser=new user({email: email, password: password})
 
-  // //CREATE A NEW VERIFICATION HERE
+  // CREATE A NEW VERIFICATION HERE
   twilioClient.verify
   .services(verificationSID)
   .verifications.create({ to: email, channel: "email" })
   .then(verification => {
     console.log("Verification email sent");
-    // res.redirect(`/verify?email=${email}`);
   })
   .catch(error => {
     console.log(error);
@@ -48,11 +66,13 @@ router.route('/verify').post((req, res) => {
   .then(verification_check => {
     console.log(verification_check.status)
     if (verification_check.status === "approved") {
-      // database.verifyUser(email);
       newUser.save()
-        .then(() => res.json('User Added'))
+        .then(() => {
+          createdb(newUser.email)
+          user.findOne({email: email}).
+            then(result => res.json(result))
+        })
         .catch(err => res.status(400).json('Error: '+err))
-      // res.redirect("users");
     } else {
       res.render("verify", {
         email: email,
