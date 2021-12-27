2ad96c2a2c211f5eefd0249e8deb2a7b52685ca1
Saral Srivastava <saralsrivastava25@gmail.com>
2021-12-27 23:05:58 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://saral:saral@cluster0.uxu98.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = process.env.MONGODB_URI;
 mongoose.connect(uri);
 const connection = mongoose.connection;
 connection.once('open', () => {
@@ -19,13 +19,15 @@ connection.once('open', () => {
 
 const signInRouter = require('./routers/user.signIn.route')
 const signUpRouter = require('./routers/user.signUp.route')
-// const buildProfileRouter = require('./routers/user,profileBuild.route')
+const buildProfileRouter = require('./routers/user,profileBuild.route')
 const newsFeedRouter = require('./routers/newsFeed.route')
+const postNews = require('./routers/postNewsfeed.route')
 
 app.use('/sign-in', signInRouter)
 app.use('/sign-up', signUpRouter)
-// app.use('/build-profile', buildProfileRouter) 
+app.use('/build-profile', buildProfileRouter) 
 app.use('/news', newsFeedRouter)
+app.use('/add', postNews)
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
